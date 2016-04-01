4a630cbbc73ab3bf4bcccea2797c766dfed711cc
lhw0304 <583139118@qq.com>
2016-04-02 03:58:30 +0800

    delete

--- yuepao/routes.js
+++
@@ -1,7 +0,0 @@
-var run = require('./controllers/callrun');
-module.exports=function(app){
-	app.get('/callrun',run.callRun);
-	app.get('/startmap',run.startMap);
-	app.get('/overmap',run.overMap);
-	app.get('/savedata',run.saveData);
-}
